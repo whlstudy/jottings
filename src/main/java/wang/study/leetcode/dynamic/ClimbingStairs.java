10397b82d1480d3ce7f839ddfe305da632461fe4
hualiwan <hualiwan@cisco.com>
2019-06-10 14:47:11 +0800

    leetcode

--- src/main/java/wang/study/leetcode/dynamic/ClimbingStairs.java
+++ src/main/java/wang/study/leetcode/dynamic/ClimbingStairs.java
@@ -1,5 +1,7 @@
 package wang.study.leetcode.dynamic;
 
+import org.junit.Test;
+
 public class ClimbingStairs {
     public int climbStairs0(int n){
         if(n == 0) return 0;
@@ -20,5 +22,24 @@ public class ClimbingStairs {
             arr[i] = arr[i-1]+arr[i-2];
         }
         return arr[n-1];
+    }
+
+    public static class HouseRobber {
+        public int rob(int[] nums) {
+            if(nums.length == 0) return 0;
+            if(nums.length == 1) return nums[0];
+            int[] dp = new int[nums.length];
+            dp[0] = nums[0];
+            dp[1] = Math.max(dp[0],nums[1]);
+            for(int i = 2;i < nums.length;i++){
+                dp[i] = Math.max(dp[i-1],dp[i-2] + nums[i]);
+            }
+            return dp[nums.length-1];
+        }
+
+        @Test
+        public void test(){
+            System.out.println(rob(new int[]{2,7,9,3,1}));
+        }
     }
 }
---
+++ src/main/java/wang/study/leetcode/dynamic/DecodeWays.java
@@ -0,0 +1,16 @@
+package wang.study.leetcode.dynamic;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DecodeWays {
+    public int numDecodings(String s) {
+        return 0;
+    }
+
+    @Test
+    public void test(){
+    }
+}
