e2ac461fb1221ebb8c676fff5e900088aca833df
hualiangwang <sa517346@mail.ustc.edu.cn>
2019-11-30 10:40:47 +0800

    4

--- src/main/java/wang/study/netty/protocol/codec/ChannelBufferByteInput.java
+++ src/main/java/wang/study/netty/protocol/codec/ChannelBufferByteInput.java
@@ -3,11 +3,63 @@ package wang.study.netty.protocol.codec;
 import io.netty.buffer.ByteBuf;
 import org.jboss.marshalling.ByteInput;
 
+import java.io.IOException;
+
 /**
  * @author whl
  * @date 2019/11/30 10:27 上午
  */
-public class ChannelBufferByteInput implements ByteInput {
-    public ChannelBufferByteInput(ByteBuf buf) {
+class ChannelBufferByteInput implements ByteInput {
+
+    private final ByteBuf buffer;
+
+    public ChannelBufferByteInput(ByteBuf buffer) {
+        this.buffer = buffer;
     }
+
+    @Override
+    public void close() throws IOException {
+        // nothing to do
+    }
+
+    @Override
+    public int available() throws IOException {
+        return buffer.readableBytes();
+    }
+
+    @Override
+    public int read() throws IOException {
+        if (buffer.isReadable()) {
+            return buffer.readByte() & 0xff;
+        }
+        return -1;
+    }
+
+    @Override
+    public int read(byte[] array) throws IOException {
+        return read(array, 0, array.length);
+    }
+
+    @Override
+    public int read(byte[] dst, int dstIndex, int length) throws IOException {
+        int available = available();
+        if (available == 0) {
+            return -1;
+        }
+
+        length = Math.min(available, length);
+        buffer.readBytes(dst, dstIndex, length);
+        return length;
+    }
+
+    @Override
+    public long skip(long bytes) throws IOException {
+        int readable = buffer.readableBytes();
+        if (readable < bytes) {
+            bytes = readable;
+        }
+        buffer.readerIndex((int) (buffer.readerIndex() + bytes));
+        return bytes;
+    }
+
 }
